c1bdf5113b3051094a5547eb1d523a3b596abfe7
Ricky Vang <vangcheemeng@yahoo.com>
2023-09-21 09:31:15 -0600

    css html

---
+++ scripts/getDates.js
@@ -0,0 +1,12 @@
+const today = new Date();
+let year = today.getFullYear();
+
+let yearElement = document.querySelector('#year')
+yearElement.textContent = year.toString();
+
+let lastModified = document.lastModified;
+let lastModifiedElement = document.querySelector(".lastModified")
+
+if (lastModifiedElement) {
+    lastModifiedElement.textContent = `Last Modified: ${lastModified}`;
+}
