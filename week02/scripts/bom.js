7cfbe3c8232bd93360d6976f14c44dc3873df42b
Ricky Vang <vangcheemeng@yahoo.com>
2023-09-26 12:35:11 -0600

    css

---
+++ week02/scripts/bom.js
@@ -0,0 +1,14 @@
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector("#list");
+const feedback = document.querySelector("#feedback");
+
+button.addEventListener("click", () => {
+    if (input.value) {
+        console.log("Clicked");
+        feedback.textContent = "";
+    } else {
+        input.focus(); //focus() will focus the input. focus() is a method.
+        feedback.textContent = "Please enter a Book and Chapter."
+    }
+});
