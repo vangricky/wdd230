90638ebf4085825676dceaba68c8bb4981a08d88
Ricky Vang <vangcheemeng@yahoo.com>
2023-09-30 21:48:22 -0600

    bom activity

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -4,11 +4,30 @@ const list = document.querySelector("#list");
 const feedback = document.querySelector("#feedback");
 
 button.addEventListener("click", () => {
-    if (input.value) {
-        console.log("Clicked");
-        feedback.textContent = "";
-    } else {
-        input.focus(); //focus() will focus the input. focus() is a method.
-        feedback.textContent = "Please enter a Book and Chapter."
-    }
+  if (input.value != "") {
+    console.log("Clicked");
+    feedback.textContent = "";
+  } else {
+    input.focus(); //focus() will focus the input. focus() is a method.
+    feedback.textContent = "Please enter a Book and Chapter.";
+  }
+
+  const li = document.createElement("li");
+  const deleteButton = document.createElement("button");
+
+  li.textContent = input.value;
+
+  deleteButton.textContent = "❌";
+
+  li.append(deleteButton);
+
+  list.append(li);
+
+  deleteButton.addEventListener("click", function () {
+    list.removeChild(li);
+    input.focus();
+  });
+
+  input.focus();
+  input.value = "";
 });
