6d4afb8707579c9abcc21ded69c71ae53d76a9d5
Ricky Vang <vangcheemeng@yahoo.com>
2023-10-07 20:04:43 -0600

    darkmode

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,28 @@
-const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
+//hamburgermenu
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
 
-hamButton.addEventListener('click', () => {
-	navigation.classList.toggle('open');
-	hamButton.classList.toggle('open');
+hamButton.addEventListener("click", () => {
+  navigation.classList.toggle("open");
+  hamButton.classList.toggle("open");
+});
+
+//nightmode
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+const anchorTags = document.querySelector("a");
+const list = document.querySelector("li");
+
+modeButton.addEventListener("click", () => {
+  if (modeButton.textContent.includes("🌙")) {
+    main.style.background = "#000";
+    main.style.color = "white";
+    modeButton.textContent = "☀️";
+    // anchorTags.style.color = "white";
+    // list.style.color = "black";
+  } else {
+    main.style.background = "white";
+    main.style.color = "black";
+    modeButton.textContent = "🌙";
+  }
 });
