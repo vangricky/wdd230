e226dc3e1aa26fc55b8c5bb87da740e40e08c8c4
Ricky Vang <vangcheemeng@yahoo.com>
2023-10-14 12:58:09 -0600

    bom local storage activity

---
+++ week03/scripts/bom-storage.js
@@ -0,0 +1,88 @@
+// Variables
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector("#list");
+const feedback = document.querySelector("#feedback");
+
+// Array
+let chaptersArray = getChaptersList() || [];
+
+// Code goes here:
+
+// button.addEventListener("click", () => {
+//   if (input.value != "") {
+//     console.log("Clicked");
+//     feedback.textContent = "";
+//   } else {
+//     input.focus(); //focus() will focus the input. focus() is a method.
+//     feedback.textContent = "Please enter a Book and Chapter.";
+//   }
+
+//   const li = document.createElement("li");
+//   const deleteButton = document.createElement("button");
+
+//   li.textContent = input.value;
+
+//   deleteButton.textContent = "❌";
+
+//   li.append(deleteButton);
+
+//   list.append(li);
+
+//   deleteButton.addEventListener("click", function () {
+//     list.removeChild(li);
+//     input.focus();
+//   });
+
+//   input.focus();
+//   input.value = "";
+// });
+
+chaptersArray.forEach((chapter) => {
+  displayList(chapter);
+});
+
+button.addEventListener("click", () => {
+  if (input.value != "") {
+    // make sure the input is not empty
+    displayList(input.value); // call the function that outputs the submitted chapter
+    chaptersArray.push(input.value); // add the chapter to the array
+    setChapterList(); // update the localStorage with the new array
+    input.value = ""; // clear the input
+    input.focus(); // set the focus back to the input
+  }
+});
+
+function displayList(item) {
+  let li = document.createElement("li");
+  let deletebutton = document.createElement("button");
+  li.textContent = item; // note the use of the displayList parameter 'item'
+  deletebutton.textContent = "❌";
+  deletebutton.classList.add("delete"); // this references the CSS rule .delete{width:fit-content;} to size the delete button
+  li.append(deletebutton);
+  list.append(li);
+  deletebutton.addEventListener("click", function () {
+    list.removeChild(li);
+    deleteChapter(li.textContent); // note this new function that is needed to remove the chapter from the array and localStorage.
+    input.focus(); // set the focus back to the input
+  });
+  console.log(
+    "I like to copy code instead of typing it out myself and trying to understand it."
+  );
+}
+
+function setChapterList() {
+  localStorage.setItem("MyFavBOMList", JSON.stringify(chaptersArray));
+}
+
+function getChaptersList() {
+  return JSON.parse(localStorage.getItem("myFavBOMList"));
+}
+
+function deleteChapter(chapter) {
+  chapter = chapter.slice(0, chapter.length - 1);
+  chaptersArray = chatpersArray.filter((item) => item !== chapter);
+  setChapterList();
+}
+
+deleteChapter();
