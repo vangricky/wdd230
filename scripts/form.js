d23cfe6711578cc81b5b21f7ffbf116e7c51d55d
Ricky Vang <vangcheemeng@yahoo.com>
2023-11-03 19:27:21 -0700

    contact us form

---
+++ scripts/form.js
@@ -0,0 +1,19 @@
+const pass1 = document.querySelector("#password");
+const pass2 = document.querySelector("#password2");
+const message = document.querySelector("#notSame");
+
+pass2.addEventListener("focusout", checkSame);
+
+function checkSame() {
+    if (pass1.value !== pass2.value) {
+        message.textContent = "Please Re-enter password. They must be the same!";
+        message.style.visibility = "show";
+		pass2.style.backgroundColor = "#fff0f3";
+		pass2.value = "";
+		pass2.focus();
+	} else {
+		message.style.display = "none";
+		pass2.style.backgroundColor = "#fff";
+		pass2.style.color = "#000";
+    }
+}
