c284ea8df7ae1828de8689c5f9a3c74c8241a34c
Ricky Vang <vangcheemeng@yahoo.com>
2023-11-30 17:15:59 -0800

    js

--- week05/scripts/prophets.js
+++ week05/scripts/prophets.js
@@ -4,7 +4,29 @@ const cards = document.querySelector("#cards");
 async function getProphetData() {
     const response = await fetch(url);
     const data = await response.json();
-    console.table(data.prophets)
+    // console.table(data.prophets)
+    displayProphets(data.prophets)
+}
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        let card = document.createElement("section");
+        let fullName = document.createElement("h2");
+        let portrait = document.createElement("img");
+
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`
+
+        portrait.setAttribute("src", prophet.imageurl);
+        portrait.setAttribute("alt", `Prophet ${prophet.name} ${prophet.lastname}`)
+        portrait.setAttribute("loading", "lazy");
+        portrait.setAttribute("width", "340");
+        portrait.setAttribute("height", "440");
+
+        card.appendChild(fullName);
+        card.appendChild(portrait);
+
+        cards.appendChild(card)
+    });
 }
 
 getProphetData()
