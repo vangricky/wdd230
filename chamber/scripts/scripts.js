f673106da143151c53ff378f086b248b024acc13
Ricky Vang <vangcheemeng@yahoo.com>
2023-12-09 16:50:34 -0700

    directory

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -22,3 +22,57 @@ if (lastModifiedElement) {
 }
 
 //directory
+
+const membersUrl =
+  "https://vangricky.github.io/wdd230/chamber/data/members.json";
+const membersCards = document.querySelector("#members-cards");
+
+async function getMemberData() {
+  const response = await fetch(membersUrl);
+  const data = await response.json();
+  // console.table(data.members)
+  displayMembers(data.members);
+}
+
+const displayMembers = (members) => {
+  members.forEach((member) => {
+    let card = document.createElement("section");
+    let name = document.createElement("h2");
+    let address = document.createElement("p");
+    let number = document.createElement("p");
+    let link = document.createElement("p");
+
+    name.textContent = `${member.name}`;
+    address.textContent = `${member.address.street}`;
+    address.style.color = "#222831";
+    number.textContent = `${member.phone}`;
+    number.style.color = "#222831";
+    link.textContent = `${member.website}`;
+    link.style.color = "#222831";
+
+    card.appendChild(name);
+    card.appendChild(address);
+    card.appendChild(number);
+    card.appendChild(link);
+
+    membersCards.appendChild(card);
+  });
+};
+
+getMemberData();
+
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+gridbutton.addEventListener("click", () => {
+  display.classList.add("grid");
+  display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList);
+
+function showList() {
+  display.classList.add("list");
+  display.classList.remove("grid");
+}
--- week05/scripts/prophets.js
+++ week05/scripts/prophets.js
@@ -1,33 +1,38 @@
-const url = "https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json"
+const url =
+  "https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json";
 const cards = document.querySelector("#cards");
 
 async function getProphetData() {
-    const response = await fetch(url);
-    const data = await response.json();
-    // console.table(data.prophets)
-    displayProphets(data.prophets)
+  const response = await fetch(url);
+  const data = await response.json();
+  // console.table(data.prophets)
+  displayProphets(data.prophets);
 }
 
 const displayProphets = (prophets) => {
-    prophets.forEach((prophet) => {
-        let card = document.createElement("section");
-        let fullName = document.createElement("h2");
-        let portrait = document.createElement("img");
-        let birth = doucment.createElement("p")
+  prophets.forEach((prophet) => {
+    let card = document.createElement("section");
+    let fullName = document.createElement("h2");
+    let portrait = document.createElement("img");
+    let dateOfBirth = document.createElement("p");
+    let placeOfBirth = document.createElement("p");
 
-        fullName.textContent = `${prophet.name} ${prophet.lastname}`
+    fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+    dateOfBirth.textContent = `Date of Birth: ${prophet.birthdate}`;
+    placeOfBirth.textContent = `Place of Birth: ${prophet.birthplace}`;
 
-        portrait.setAttribute("src", prophet.imageurl);
-        portrait.setAttribute("alt", `Prophet ${prophet.name} ${prophet.lastname}`)
-        portrait.setAttribute("loading", "lazy");
-        portrait.setAttribute("width", "340");
-        portrait.setAttribute("height", "440");
+    portrait.setAttribute("src", prophet.imageurl);
+    portrait.setAttribute("alt", `Prophet ${prophet.name} ${prophet.lastname}`);
+    portrait.setAttribute("loading", "lazy");
+    portrait.setAttribute("width", "340");
+    portrait.setAttribute("height", "440");
 
-        card.appendChild(fullName);
-        card.appendChild(portrait);
+    card.appendChild(fullName);
+    card.appendChild(portrait);
+    card.appendChild(dateOfBirth);
+    card.appendChild(placeOfBirth);
+    cards.appendChild(card);
+  });
+};
 
-        cards.appendChild(card)
-    });
-}
-
-getProphetData()
+getProphetData();
