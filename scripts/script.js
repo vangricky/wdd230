8255aea8937eafb3b0ac5d98f87f98df4d2bee0b
Ricky Vang <vangcheemeng@yahoo.com>
2023-12-09 17:20:41 -0700

    js index

--- scripts/script.js
+++ scripts/script.js
@@ -40,3 +40,38 @@ if (numVisits !== 0) {
 numVisits++;
 
 localStorage.setItem("numVisits-ls", numVisits);
+
+//dynamic weather
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
+
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?lat=43.82&lon=-111.80&appid=83cfd6376f4f3dc4b8900a19166fa46f&units=imperial";
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      displayResults(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+apiFetch();
+
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  let desc = data.weather[0].description;
+  weatherIcon.setAttribute("src", iconsrc);
+  weatherIcon.setAttribute("alt", "weather icon");
+  weatherIcon.setAttribute("loading", "lazy");
+  captionDesc.textContent = `${desc}`;
+}
